268cd2b9230df98cff68cc1438e19fdd8c210553
Alexander Hering <dev.hering@gmail.com>
2024-08-16 17:56:38 +0200

    added output linkage into installation step

--- install_webui.sh
+++ install_webui.sh
@@ -2,5 +2,6 @@
 
 source /comfyui-container/venv/bin/activate
 cd /comfyui-container/comfyui
+ln -sf /comfyui-container/stable_diffusion_output/ comfyui/output
 pip install --no-cache-dir torchvision==0.17.1+cu121 torchaudio==2.2.1+cu121 torch==2.2.1+cu121 xformers --extra-index-url https://download.pytorch.org/whl/cu121
 pip install --no-cache-dir -r requirements.txt
